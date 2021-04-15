88f2339ba666b171d5ceb0e4efcc83098e897a13
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-04-15 11:18:09 +0900

    map displays

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect }from "react";
+import ReactMapGL, {Marker, Popup} from "react-map-gl";
+import * as stationData from "./data/stations.json";
+import './index.css';
 
 function App() {
+  // viiewport
+  const [viewport, setViewport] = useState({
+    latitude: 35.6762,
+    longitude: 139.6503,
+    zoom: 10,
+    width: "100vw",
+    height: "100vh"
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ReactMapGL
+      {...viewport} 
+      mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      mapStyle="mapbox://styles/wallace-a/ckn9vqev925hp17pg7zu5ea1j"
+      onViewportChange={viewport =>{
+        setViewport(viewport);
+      }}
+      >
+      
+      </ReactMapGL>
     </div>
   );
 }
